42c3b664283ce2fd9322b20272be5caa9fdd67ca
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-10-25 10:08:34 +0600

    left Side nav added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrnnzi6zXiEz7CaejS-nQhxv02bH-pdzQ",
+  authDomain: "studics.firebaseapp.com",
+  projectId: "studics",
+  storageBucket: "studics.appspot.com",
+  messagingSenderId: "432237798318",
+  appId: "1:432237798318:web:ff76f11cbf0ded16775e3f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
